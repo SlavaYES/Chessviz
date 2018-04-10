3b18bd3a45d48f2bc01148866815e823a8745186
SlavaYES <voen1999@gmail.com>
2018-04-10 14:39:44 +0700

    Different directories

--- Makefile
+++ Makefile
@@ -6,23 +6,18 @@ CFLAGS=-Wall -Werror -c
 
 #Задаю путь src
 SRC=src/
-#Задаю исходники файлы
-SOURCES=$(SRC)main.c $(SRC)conios.c
 
 #Задаю путь build
 dir=build/
 #Задаю обьектные файлы
-objects=$(dir)main.o $(dir)conios.o
+objects=$(dir)main.o $(dir)conios.o $(dir)board_read.o $(dir)board_print_plain.o $(dir)board.o
 
 #Задаю путь bin
 BIN=bin/
 #Задаю испольняемый файл
 EXECUTABLE=$(BIN)main
 
-#Задаю путь include
-INCLUDE= -MM -I./include
-
-all: bin build $(BIN)main
+all: bin build $(EXECUTABLE)
 
 $(EXECUTABLE): $(objects)
 	$(CC) $(objects) -o $@
@@ -30,8 +25,19 @@ $(EXECUTABLE): $(objects)
 $(dir)main.o: $(SRC)main.c $(SRC)conios.h
 	$(CC) $(CFLAGS) $(SRC)main.c -o $@
 
+
+$(dir)board_print_plain.o: $(SRC)board_print_plain.c $(SRC)board_print_plain.h $(SRC)conios.h 
+	$(CC) $(CFLAGS) $(SRC)board_print_plain.c -o $@
+
 $(dir)conios.o: $(SRC)conios.c $(SRC)conios.h
 	$(CC) $(CFLAGS) $(SRC)conios.c -o $@
+	
+$(dir)board_read.o: $(SRC)board_read.c $(SRC)board_read.h
+	$(CC) $(CFLAGS) $(SRC)board_read.c -o $@
+	
+
+$(dir)board.o: $(SRC)board.c $(SRC)board.h
+	$(CC) $(CFLAGS) $(SRC)board.c -o $@
 
 build:
 	mkdir build
@@ -40,4 +46,4 @@ bin:
 src:
 	mkdir src
 clean:
-	-rm -rf build bin
+	-rm -rf build
--- src/main.c
+++ src/main.c
@@ -2,29 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include "conios.h"
+#include "board_read.h"
+#include "board_print_plain.h"
+#include "board.h"
 
-const int n=9, m=9+1;
-
-void swap(char *x, char *y);
-void outBoard(char a[n][m], char *);
-int check(char a[n][m], char *, int *, int);
-
+int n=9, m=9+1;
 
 int main()
 {
     char a[n][m];
-    int i;
-    FILE *lone;
-    
-    lone=fopen("Chess.txt", "r");
-    a[0][0]='\0';
-    i=1;
-    while (!feof(lone)) {
-	fread(a[i], sizeof(a[1]), 1, lone);
-	i++;
-    }
-    fclose(lone);
-    
+    bread(a);
     char xod[6]="\0", ch='\0';
     int h[5];
     int flag=0;//Флаг 0- Белый, Флаг 1- Черный
@@ -34,58 +21,9 @@ int main()
 	if (flag) {
 	    printf("Ход Черного: ");
 	} else printf("Ход Белого: ");
+	
 	scanf("%s", xod);
-    
-        for (i=0; i<5; i++) {
-	    switch (xod[i]) {
-	    case 'a':
-	    case '1':
-		h[i]=1;
-		break;
 	
-	    case 'b':
-	    case '2':
-		h[i]=2;
-		break;
-
-	    case 'c':
-	    case '3':
-		h[i]=3;
-		break;
-
-	    case 'd':
-	    case '4':
-		h[i]=4;
-		break;
-
-	    case 'e':
-	    case '5':
-		h[i]=5;
-		break;
-	
-	    case 'f':
-	    case '6':
-		h[i]=6;
-		break;
-	
-	    case 'g':
-	    case '7':
-		h[i]=7;
-		break;
-	
-	    case 'h':
-	    case '8':
-		h[i]=8;
-		break;
-	
-	    case '-':
-		h[2]=-1;
-		break;
-	    case 'x':
-		h[2]=-2;
-		break;
-	    }
-	}
 	if (check(a, xod, h, flag)) {
 	    outBoard(a, xod);
 	    flag=1-flag;
@@ -97,61 +35,6 @@ int main()
     getch();
     return 0;
 }
-
-void swap(char *x, char *y)
-{
-    char tmp=*x; *x=*y; *y=tmp;
-}
-
-void outBoard(char a[n][m], char *xod)
-{
-    int i, j;
-    printf("\n");
-    clrscr();
-    printf("Предыдущий Ход:"); puts(xod);
-    printf("    A B C D E F G H\n\n");
-    for (i=1; i<n; i++) {
-	printf("\x1b[0m%d", i);
-	for (j=0; j<m; j++) {
-	    if (a[i][j]>='A' && a[i][j]<='Z') printf("\x1b[37m");
-		else printf("\x1b[30m");
-	    printf("%2c", a[i][j]);
-	}
-    }
-    printf("\x1b[0m\n");
-}
-
-int check(char a[n][m], char *xod, int *h, int flag)
-{
-    char tmp0=a[h[1]][h[0]];
-    char tmp1=a[h[4]][h[3]];
-    /*ПРОВЕРКА НА ЧЕРНОТУ*/
-    if (!flag) {
-	if (tmp0>='A' && tmp0<='Z') {
-	    if (tmp0=='P') {
-		if (h[0]==h[3]) {
-		    if (h[2]==-1 && tmp1==' ') {
-			swap(&a[h[1]][h[0]], &a[h[4]][h[3]]);
-			return 1;
-		    }
-		}
-	    }
-	}
-    } else {
-	if (tmp0>='a' && tmp0<='z') {
-	    if (tmp0=='p') {
-		if (h[0]==h[3]) {
-		    if (h[2]==-1 && tmp1==' ') {
-			swap(&a[h[1]][h[0]], &a[h[4]][h[3]]);
-			return 1;
-		    }
-		}
-	    }
-	}
-    }
-    return 0;
-}
-
 
 
 
